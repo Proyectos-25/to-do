e8cb5106f792cc2749b8afde988999789af83f1e
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-12-22 20:37:07 -0300

    Agrego footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import ToDoWrapper from './components/ToDoWrapper'
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
 <div className="App">
   <ToDoWrapper />
+  <Footer />
 </div>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Developed with ⌨️ by <a href="https://github.com/na7hk3r" target="_blank" rel="noopener noreferrer">na7hk3r</a>
+    </footer>
+  )
+}
+
+export default Footer
